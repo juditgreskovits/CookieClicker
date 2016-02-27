8bfd995e73f6067be275752673d3d2d33461a049
Lukas van Driel <lukas@q42.nl>
2016-02-27 11:11:33 +0000

    use a normal href #6

--- client/components/layouts/NotFound.jsx
+++ client/components/layouts/NotFound.jsx
@@ -1,13 +1,9 @@
 NotFound = React.createClass({
-  goHome() {
-    FlowRouter.go('/');
-  },
-
   render() {
     return (
       <div id="not-found" className="container">
         <h2>Sorry, route not found</h2>
-        <p><a className="btn btn-primary btn-lg" href="#" role="button" onClick={ this.goHome }>Home</a></p>
+        <p><a className="btn btn-primary btn-lg" href="/" role="button">Home</a></p>
       </div>
     )
   }
